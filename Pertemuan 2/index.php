b7f0e94a1362933abed799a9c98aa54b3733c418
ginanjarademulyana <dorion.official@gmail.com>
2021-02-05 14:25:56 +0700

    Membuat file index.php dan mengenal sintaks dasar pemrograman

---
+++ Pertemuan 2/index.php
@@ -0,0 +1,59 @@
+<?php
+// -------------------------------------------------------------------
+// Ginanjar Ade Mulyana
+// 153040116
+// https://github.com/ginanjarademulyana/
+// Pertemuan 2 (5 Januari 2021)
+// Materi minggu ini mempelajari dan mengenai sintaks dasar pemrograman pada PHP
+//--------------------------------------------------------------------
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam html
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// Tidak boleh di awali dengan angka, tapi boleh mengandung angka
+
+// $nama = "Ginanjar Ade Mulyana";
+// echo 'Halo, nama saya $nama';
+
+// Operator
+// Aritmatika
+// + - * / %
+//  $x = 10;
+//  $y = 20;
+//  echo $x * $y;
+
+// Penggabungan string / concatenation / contact
+// .
+// $nama_depan = "Ginanjar";
+// $nama_belakang = "Ade Mulyana";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assignment
+// =, +=, -=, /=, %=, .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+// $nama = "Ginanjar";
+// $nama = "Ade Mulyana";
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// identitas
+// ===, !==
+// var_dump(1 === "1");
+
+// Logika
+// &&, ||, !
+// $x = 10;
+// var_dump($x < 20 && $x % 2 == 0);
