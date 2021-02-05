5e2fa27582812a19caef6f5c3522f77f2a8833dc
ginanjarademulyana <dorion.official@gmail.com>
2021-02-05 15:49:29 +0700

    membuat branch laptop untuk pengerjaan di laptop dan membuat file function.php

---
+++ Pertemuan 4/function.php
@@ -0,0 +1,8 @@
+<?php
+// -------------------------------------------------------------------
+// Ginanjar Ade Mulyana
+// 153040116
+// https://github.com/ginanjarademulyana/pw2021_153040116/tree/main/Pertemuan%204
+// Pertemuan 3 (5 Januari 2021)
+// Materi minggu ini mempelajari dan mengenai sintaks dasar pemrograman pada PHP
+//--------------------------------------------------------------------
