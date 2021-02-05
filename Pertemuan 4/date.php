57e82f7fcb9fd9b1027c11b2564dcf573df63ecb
ginanjarademulyana <dorion.official@gmail.com>
2021-02-05 15:39:50 +0700

    mengenal cara menampilkan tanggal tertentu dan menghitung detik tertentu

--- Pertemuan 4/date.php
+++ Pertemuan 4/date.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // -------------------------------------------------------------------
 // Ginanjar Ade Mulyana
 // 153040116
@@ -6,3 +6,35 @@
 // Pertemuan 3 (5 Januari 2021)
 // Materi minggu ini mempelajari dan mengenai sintaks dasar pemrograman pada PHP
 //--------------------------------------------------------------------
+
+
+// Date
+echo "(Untuk menampilkan Tanggal Tertentu) - ";
+echo date("l, j M Y");
+
+
+echo "<br>";
+echo "<br>";
+
+// Time
+// UNIX Timestamp / EPOCH time
+// detik yang sudah berlalu sejak 1 Januari 1970
+// echo time();
+echo "hari dimana 100 hari dari hari coding ini dibuat = ";
+echo date("l", time() - 60 * 60 * 24 * 100);
+
+echo "<br>";
+echo "<br>";
+
+// mktime
+// membuat sendiri detik
+// mk time(0,0,0,0,0,0)
+// jam , menit, detik, bulan, hari (tanggal), tahun
+echo date("l", mktime(0, 0, 0, 5, 23, 1997));
+echo " - input array";
+
+echo "<br>";
+
+// strtotime
+echo date("l", strtotime("23 May 1997"));
+echo " - input string";
