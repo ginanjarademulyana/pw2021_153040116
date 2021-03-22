0f12c0890acd09e33bdecdc966705e40be896d91
ginanjarademulyana <dorion.official@gmail.com>
2021-03-22 18:54:39 +0700

    Membuat latihan3.php dan belajar menampilkan array dengan daftar

---
+++ Pertemuan 5/Latihan3.php
@@ -0,0 +1,29 @@
+<?php 
+
+$mahasiswa = [
+  ["Ginanjar Ade Mulyana", "153040116", "Teknik Informatika", "dorion.official@gmail.com"],
+  ["Dewi Wisuda Wardani", "153040117", "Teknik Informatika", "dewi.official@gmail.com"]
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+<body>
+  <h1>Daftar Mahasiswa</h1>
+  <?php foreach ($mahasiswa as $mhs) : ?>
+  <ul>
+    <li>Nama : <?= $mhs[0] ?></li>
+    <li>NRP : <?= $mhs[1] ?></li>
+    <li>Jurusan : <?= $mhs[2] ?></li>
+    <li>Email : <?= $mhs[3] ?></li>
+  </ul>
+  <?php endforeach; ?>
+</body>
+</html>
