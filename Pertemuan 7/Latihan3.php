da70fb75301afd73a6e4a83fdd419aeeef9a3645
ginanjarademulyana <dorion.official@gmail.com>
2021-03-23 19:34:40 +0700

    Membuat file latihan 3 dan 4 untuk latihan membuat halaman login

---
+++ Pertemuan 7/Latihan3.php
@@ -0,0 +1,32 @@
+<?php
+// -------------------------------------------------------------------
+// Ginanjar Ade Mulyana
+// 153040116
+// https://github.com/ginanjarademulyana/pw2021_153040116/tree/main/Pertemuan%207
+// Pertemuan 7 (19 Maret 2021)
+// Materi minggu ini mempelajari dan mengenai Get & Post
+//--------------------------------------------------------------------
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>POST</title>
+</head>
+
+<body>
+  <h1>Halo Silahkan Masukan Nama Anda!</h1>
+  <form action="Latihan4.php" method="post">
+    Masukan Nama :
+    <input type="text" name="nama" id="">
+    <br>
+    <button type="submit" name="submit"> Kirim </button>
+  </form>
+
+</body>
+
+</html>
