ca2f72536740cd7aeed8c6a361f06f24c21b1474
ginanjarademulyana <dorion.official@gmail.com>
2021-04-11 14:20:24 +0700

    menyelesaikan halaman admin.php

--- Praktikum/Tugas5/Latihan5a/php/admin.php
+++ Praktikum/Tugas5/Latihan5a/php/admin.php
@@ -12,7 +12,7 @@ $produk = produk("SELECT * FROM produk");
 
 <head>
   <meta charset="UTF-8">
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="../css/style.css">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
@@ -22,37 +22,38 @@ $produk = produk("SELECT * FROM produk");
   <div class="small-container">
     <h2 class="title">Admin Page</h2>
     <div class="row">
-      <div class="col-4">
-        <img src="../assets/img/products/product-1.jpg" alt="">
-      </div>
-      <table border="1" cellpadding="13" cellspacing="0">
-        <tr>
-          <th>#</th>
-          <th>Options</th>
-          <th>Gambar</th>
-          <th>Kode Barang</th>
-          <th>Nama Barang</th>
-          <th>Stok Barang</th>
-          <th>Harga</th>
-        </tr>
-        <?php $i = 1; ?>
-        <?php foreach ($produk as $prod) : ?>
+      <div class="table">
+        <table border="1" cellpadding="13" cellspacing="0">
           <tr>
-            <td><?= $i; ?></td>
-            <td>
-              <a href=""><button>Ubah</button></a>
-              <a href=""><button>Hapus</button></a>
-            </td>
-            <td><img src="../assets/img/products/<?= $prod['gambar_produk']; ?>"></td>
-
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <th>#</th>
+            <th>Options</th>
+            <th>Gambar</th>
+            <th>Kode Barang</th>
+            <th>Nama Barang</th>
+            <th>Stok Barang</th>
+            <th>Harga</th>
           </tr>
-          <?php $i++; ?>
-        <?php endforeach; ?>
-      </table>
+          <?php $i = 1; ?>
+          <?php foreach ($produk as $prod) : ?>
+            <tr>
+              <td><?= $i; ?></td>
+              <td>
+                <a href=""><button>Ubah</button></a>
+                <a href=""><button>Hapus</button></a>
+              </td>
+              <td>
+                <img src="../assets/img/products/<?= $prod['gambar_produk']; ?>">
+              </td>
+              <td>
+                RDSTR00<?= $prod['kode_barang']; ?> </td>
+              <td><?= $prod['nama_barang']; ?></td>
+              <td><?= $prod['stok_barang']; ?></td>
+              <td><?= $prod['harga']; ?></td>
+            </tr>
+            <?php $i++; ?>
+          <?php endforeach; ?>
+        </table>
+      </div>
     </div>
   </div>
 </body>
