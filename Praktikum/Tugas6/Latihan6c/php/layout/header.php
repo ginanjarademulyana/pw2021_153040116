fe1aeb0d43a2dd95dadae4e698ec957b0fe275f8
ginanjarademulyana <dorion.official@gmail.com>
2021-04-21 22:49:26 +0700

    duplicate latihan6b ke c

---
+++ Praktikum/Tugas6/Latihan6c/php/layout/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title> Red Store </title>
+  <link rel="stylesheet" href="../css/style.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+  <div class="container">
+    <div class="navbar">
+      <div class="logo">
+        <a href="../index.php"><img src="../assets/img/icons/logo.png" width="125px"></a>
+      </div>
+      <nav>
+        <ul id="MenuItems">
+          <li><a href="../index.php">Home</a></li>
+          <li><a href="">Products</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Contact</a></li>
+          <li><a href="login.php">Account</a></li>
+        </ul>
+      </nav>
+      <a href=""><img src="../assets/img/icons/cart.png" width="30px" height="30px"></a>
+      <a href=""><img src="../assets/img/icons/menu.png" class="menu-icon" onclick="menutoggle()"></a>
+    </div>
+  </div>
